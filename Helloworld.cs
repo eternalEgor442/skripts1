aec702f778392383d1bf14f2773854d5f05ac6c3
eternalEgor442 <fomin.egor.msk@gmail.com>
2025-04-07 20:37:13 +0300

    version1

---
+++ Helloworld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helloworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //œ»ÿ≈“ œ–»¬≈“ Ã»–
+        print("Helloworld");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
