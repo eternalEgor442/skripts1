d28f0c47bb96ffbc1355fb690fc8c7740b75f351
eternalEgor442 <fomin.egor.msk@gmail.com>
2025-04-13 17:03:06 +0300

    npc

---
+++ NPC.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{//«ƒќ–ќ¬№≈ NPC
+    public int Health = 5;
+
+    //уровень npc
+    public int level = 1;
+
+    //скорость NPC
+    public float speed = 1.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Vector3 newPosition;
+        newPosition = transform.position;
+        newPosition.z += speed * Time.deltaTime;
+        transform.position = newPosition;
+
+
+
+    }
+}
